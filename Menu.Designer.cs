14df6c715a3584c18f1d693743d96b74684b0f73
Abraham <Abraham@NEY>
2026-02-28 23:22:49 -0700

    Revert "formsSizable"
    This reverts commit c636725153a6537263525f7f840bf60989a00055.

--- Menu.Designer.cs
+++ Menu.Designer.cs
@@ -56,11 +56,9 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.groupBox3.Controls.Add(this.pictureBox1);
-            this.groupBox3.Location = new System.Drawing.Point(9, 214);
-            this.groupBox3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox3.Location = new System.Drawing.Point(12, 264);
             this.groupBox3.Name = "groupBox3";
-            this.groupBox3.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.groupBox3.Size = new System.Drawing.Size(477, 318);
+            this.groupBox3.Size = new System.Drawing.Size(636, 391);
             this.groupBox3.TabIndex = 2;
             this.groupBox3.TabStop = false;
             // 
@@ -71,10 +69,9 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.pictureBox1.BackgroundImage = global::ConsultorioMedico.Properties.Resources.IMG_7148;
             this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.pictureBox1.Location = new System.Drawing.Point(4, 2);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pictureBox1.Location = new System.Drawing.Point(6, 3);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(472, 310);
+            this.pictureBox1.Size = new System.Drawing.Size(630, 382);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
@@ -89,11 +86,9 @@
             this.groupBox4.Controls.Add(this.label3);
             this.groupBox4.Controls.Add(this.monthCalendar1);
             this.groupBox4.Controls.Add(this.label2);
-            this.groupBox4.Location = new System.Drawing.Point(490, 216);
-            this.groupBox4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox4.Location = new System.Drawing.Point(654, 266);
             this.groupBox4.Name = "groupBox4";
-            this.groupBox4.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.groupBox4.Size = new System.Drawing.Size(308, 314);
+            this.groupBox4.Size = new System.Drawing.Size(410, 386);
             this.groupBox4.TabIndex = 3;
             this.groupBox4.TabStop = false;
             // 
@@ -103,10 +98,9 @@
             this.label5.AutoSize = true;
             this.label5.BackColor = System.Drawing.Color.Transparent;
             this.label5.Font = new System.Drawing.Font("Microsoft YaHei UI", 7.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(20, 76);
-            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Location = new System.Drawing.Point(26, 93);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(83, 16);
+            this.label5.Size = new System.Drawing.Size(97, 19);
             this.label5.TabIndex = 4;
             this.label5.Text = "81 1277 1231";
             // 
@@ -116,10 +110,9 @@
             this.label4.AutoSize = true;
             this.label4.BackColor = System.Drawing.Color.Transparent;
             this.label4.Font = new System.Drawing.Font("Microsoft YaHei UI", 7.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(16, 30);
-            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Location = new System.Drawing.Point(21, 37);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(292, 16);
+            this.label4.Size = new System.Drawing.Size(355, 19);
             this.label4.TabIndex = 3;
             this.label4.Text = "Av. Simón Bolívar 919, Mitras Sur, 64020 Monterrey";
             // 
@@ -129,18 +122,16 @@
             this.label3.AutoSize = true;
             this.label3.BackColor = System.Drawing.Color.Transparent;
             this.label3.Font = new System.Drawing.Font("Microsoft YaHei", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(20, 59);
-            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Location = new System.Drawing.Point(26, 73);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(66, 17);
+            this.label3.Size = new System.Drawing.Size(80, 19);
             this.label3.TabIndex = 2;
             this.label3.Text = "Teléfono:";
             // 
             // monthCalendar1
             // 
             this.monthCalendar1.Anchor = System.Windows.Forms.AnchorStyles.Top;
-            this.monthCalendar1.Location = new System.Drawing.Point(56, 124);
-            this.monthCalendar1.Margin = new System.Windows.Forms.Padding(7, 7, 7, 7);
+            this.monthCalendar1.Location = new System.Drawing.Point(74, 152);
             this.monthCalendar1.Name = "monthCalendar1";
             this.monthCalendar1.TabIndex = 1;
             // 
@@ -150,10 +141,9 @@
             this.label2.AutoSize = true;
             this.label2.BackColor = System.Drawing.Color.Transparent;
             this.label2.Font = new System.Drawing.Font("Microsoft YaHei", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(18, 15);
-            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Location = new System.Drawing.Point(24, 18);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(69, 17);
+            this.label2.Size = new System.Drawing.Size(84, 19);
             this.label2.TabIndex = 0;
             this.label2.Text = "Dirección:";
             // 
@@ -167,11 +157,9 @@
             this.groupBox2.Controls.Add(this.btnExpedientes);
             this.groupBox2.Controls.Add(this.btnCitas);
             this.groupBox2.Controls.Add(this.btnPacientes);
-            this.groupBox2.Location = new System.Drawing.Point(9, 94);
-            this.groupBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox2.Location = new System.Drawing.Point(12, 116);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.groupBox2.Size = new System.Drawing.Size(791, 118);
+            this.groupBox2.Size = new System.Drawing.Size(1055, 145);
             this.groupBox2.TabIndex = 1;
             this.groupBox2.TabStop = false;
             // 
@@ -184,10 +172,9 @@
             this.btnMedicos.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.btnMedicos.Font = new System.Drawing.Font("Palatino Linotype", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnMedicos.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(13)))), ((int)(((byte)(110)))), ((int)(((byte)(253)))));
-            this.btnMedicos.Location = new System.Drawing.Point(571, 30);
-            this.btnMedicos.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnMedicos.Location = new System.Drawing.Point(761, 37);
             this.btnMedicos.Name = "btnMedicos";
-            this.btnMedicos.Size = new System.Drawing.Size(104, 36);
+            this.btnMedicos.Size = new System.Drawing.Size(139, 44);
             this.btnMedicos.TabIndex = 2;
             this.btnMedicos.Text = "Medicos";
             this.btnMedicos.UseVisualStyleBackColor = false;
@@ -201,10 +188,9 @@
             this.btnExpedientes.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.btnExpedientes.Font = new System.Drawing.Font("Palatino Linotype", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnExpedientes.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(13)))), ((int)(((byte)(110)))), ((int)(((byte)(253)))));
-            this.btnExpedientes.Location = new System.Drawing.Point(334, 70);
-            this.btnExpedientes.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnExpedientes.Location = new System.Drawing.Point(445, 86);
             this.btnExpedientes.Name = "btnExpedientes";
-            this.btnExpedientes.Size = new System.Drawing.Size(121, 36);
+            this.btnExpedientes.Size = new System.Drawing.Size(161, 44);
             this.btnExpedientes.TabIndex = 1;
             this.btnExpedientes.Text = "Expedientes";
             this.btnExpedientes.UseVisualStyleBackColor = false;
@@ -219,10 +205,9 @@
             this.btnCitas.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.btnCitas.Font = new System.Drawing.Font("Palatino Linotype", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnCitas.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(13)))), ((int)(((byte)(110)))), ((int)(((byte)(253)))));
-            this.btnCitas.Location = new System.Drawing.Point(342, 17);
-            this.btnCitas.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnCitas.Location = new System.Drawing.Point(456, 21);
             this.btnCitas.Name = "btnCitas";
-            this.btnCitas.Size = new System.Drawing.Size(104, 36);
+            this.btnCitas.Size = new System.Drawing.Size(139, 44);
             this.btnCitas.TabIndex = 1;
             this.btnCitas.Text = "Citas";
             this.btnCitas.UseVisualStyleBackColor = false;
@@ -237,10 +222,9 @@
             this.btnPacientes.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.btnPacientes.Font = new System.Drawing.Font("Palatino Linotype", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnPacientes.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(13)))), ((int)(((byte)(110)))), ((int)(((byte)(253)))));
-            this.btnPacientes.Location = new System.Drawing.Point(91, 30);
-            this.btnPacientes.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnPacientes.Location = new System.Drawing.Point(121, 37);
             this.btnPacientes.Name = "btnPacientes";
-            this.btnPacientes.Size = new System.Drawing.Size(104, 36);
+            this.btnPacientes.Size = new System.Drawing.Size(139, 44);
             this.btnPacientes.TabIndex = 0;
             this.btnPacientes.Text = "Pacientes";
             this.btnPacientes.UseVisualStyleBackColor = false;
@@ -253,11 +237,9 @@
             this.groupBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(13)))), ((int)(((byte)(110)))), ((int)(((byte)(253)))));
             this.groupBox1.BackgroundImage = global::ConsultorioMedico.Properties.Resources.Captura_de_pantalla_2026_02_28_150022;
             this.groupBox1.Controls.Add(this.lblCentroMedico);
-            this.groupBox1.Location = new System.Drawing.Point(9, 6);
-            this.groupBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox1.Location = new System.Drawing.Point(12, 7);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.groupBox1.Size = new System.Drawing.Size(791, 84);
+            this.groupBox1.Size = new System.Drawing.Size(1055, 103);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             // 
@@ -270,24 +252,21 @@
             this.lblCentroMedico.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.lblCentroMedico.Font = new System.Drawing.Font("Candara", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblCentroMedico.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.lblCentroMedico.Location = new System.Drawing.Point(214, 17);
-            this.lblCentroMedico.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblCentroMedico.Location = new System.Drawing.Point(285, 21);
             this.lblCentroMedico.Name = "lblCentroMedico";
-            this.lblCentroMedico.Size = new System.Drawing.Size(397, 44);
+            this.lblCentroMedico.Size = new System.Drawing.Size(489, 55);
             this.lblCentroMedico.TabIndex = 0;
             this.lblCentroMedico.Text = "Centro Médico Harmonia";
             // 
             // Menu
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(809, 540);
+            this.ClientSize = new System.Drawing.Size(1079, 665);
             this.Controls.Add(this.groupBox4);
             this.Controls.Add(this.groupBox3);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Menu";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Consultorio Medico";
